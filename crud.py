251d95e1a8afd884eadf9b6635b3692fc6a75125
anza <zapokami@gmail.com>
2023-10-13 20:00:10 +0300

    crud docstrings

--- crud.py
+++ crud.py
@@ -4,6 +4,7 @@ import models
 
 
 def commit_to_db(db: Session, db_model: models.Base) -> None:
+    """Commiting to db function"""
     db.add(db_model)
     db.commit()
     db.refresh(db_model)
@@ -13,7 +14,7 @@ def create_quiz(
     db: Session,
     quiz_data: dict,
 ) -> models.QuizQuestion:
-
+    """Creating question in db"""
     db_quiz_question = models.QuizQuestion(
         text=quiz_data['question'],
         answer=quiz_data['answer']
@@ -23,10 +24,12 @@ def create_quiz(
 
 
 def get_question_by_text(text: str, db: Session):
+    """Function that gets qestion from db by text"""
     return db.query(models.QuizQuestion).filter(
                 models.QuizQuestion.text == text).first()
 
 
 def get_last_question(db: Session):
+    """Function that gets last question from db"""
     return db.query(models.QuizQuestion).order_by(
             models.QuizQuestion.time_created.desc()).first()
